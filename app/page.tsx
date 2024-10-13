342c4ed1aabf1c055567b89d69c7488a837e9c71
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-10-13 17:25:53 -0300

    ✨ feat(search): component working

--- app/page.tsx
+++ app/page.tsx
@@ -5,29 +5,19 @@ import { useState } from "react";
 import Header from "../components/header";
 import { Card } from "../components/card";
 import { Search } from "../components/search";
+import { feirasArray } from "../mock/feiras";
 
 export default function HomePage() {
-  const mock: TypeFeira = {
-    id: "123",
-    name: "Lorem ipsum dolor sit amet.",
-    address: "R. Dos Bobos, 00",
-    number: "123",
-    city: "Lorem ipsum dolor sit amet.",
-    description: "Lorem ipsum dolor sit amet.",
-    image: "https://via.placeholder.com/331x100",
-    feiranteID: "123",
-    date: new Date(),
-    tags: [
-      { id: "123", name: "Tag 1" },
-      { id: "123", name: "Tag 2" },
-    ],
-    nota: 3.5,
-  };
+  const [mockData, setMockData] = useState(feirasArray);
   return (
     <main className="h-screen w-screen bg-[url('https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/02/google-maps-e1707316052388.png?w=1200&h=900&crop=1')]">
       <Header />
-      <Search />
-      <Card mock={mock} />
+      <div className="flex flex-col gap-5">
+        <Search mock={feirasArray} setMockData={setMockData} />
+        {mockData.map((mock) => (
+          <Card mock={mock} />
+        ))}
+      </div>
     </main>
   );
 }
--- components/search.tsx
+++ components/search.tsx
@@ -1,7 +1,68 @@
-export function Search() {
+import { Autocomplete, IconButton, TextField } from "@mui/material";
+import { Filter, SearchIcon, SortAsc } from "lucide-react";
+import { useState } from "react";
+
+interface Props {
+  mock: TypeFeira[];
+  setMockData: (mock: TypeFeira[]) => void;
+}
+
+export function Search({ mock, setMockData }: Props) {
+  const [input, setInput] = useState("");
   return (
-    <div>
-      <input type="text" placeholder="Pesquisar" />
+    <div className="flex w-full sticky top-0 z-50 rounded-2xl overflow-hidden border border-black bg-white">
+      {/* TODO fix border on hover */}
+      <Autocomplete
+        freeSolo
+        disableClearable
+        sx={{
+          width: "100%",
+        }}
+        options={mock.map((option) => option.name)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            sx={{
+              width: "100%",
+            }}
+            label="Search input"
+            className="w-full rounded-xl overflow-hidden "
+            slotProps={{
+              input: {
+                ...params.InputProps,
+                type: "search",
+                endAdornment: (
+                  <div className="flex w-fit absolute right-0 top-0 h-full bg-secundaria-fraca items-center">
+                    <IconButton
+                      onClick={() => {
+                        setMockData(
+                          mock.filter((option) =>
+                            option.name
+                              .toLowerCase()
+                              .includes(input.toLowerCase())
+                          )
+                        );
+                      }}
+                      className="h-fit"
+                    >
+                      <SearchIcon className="text-white" />
+                    </IconButton>
+                    <IconButton className="h-fit">
+                      <Filter className="text-white" />
+                    </IconButton>
+                    <IconButton className="h-fit">
+                      <SortAsc className="text-white" />
+                    </IconButton>
+                  </div>
+                ),
+              },
+            }}
+          />
+        )}
+        onInputChange={(event, newInputValue) => {
+          setInput(newInputValue);
+        }}
+      />
     </div>
   );
 }
