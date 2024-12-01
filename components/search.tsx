78d9be6b159cef003dcf4d5f0c163a16c55a0177
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-12-01 18:39:22 -0300

    💄 style(search): better spacing between header

--- components/search.tsx
+++ components/search.tsx
@@ -10,7 +10,7 @@ interface Props {
 export function Search({ mock, setMockData }: Props) {
   const [input, setInput] = useState("")
   return (
-    <div className="flex sticky top-1 z-50 rounded-2xl mb-5 mx-5">
+    <div className="flex sticky top-1 z-50 rounded-2xl mb-5 mx-5 mt-1">
       <Autocomplete
         freeSolo
         sx={{
