179423928bfaea6c953e9632598088fddeb8dc2e
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2024-12-02 13:24:21 -0300

    💄 style: icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import '../styles/globals.css';
+import { icons } from "lucide-react";
+import "../styles/globals.css";
 
 export const metadata = {
-  title: 'Minha Feira',
-  description: 'Website da Minha Feira.',
-}
+  title: "Minha Feira",
+  description: "Website da Minha Feira.",
+  icons: {
+    icon: "/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
     </html>
-  )
+  );
 }
